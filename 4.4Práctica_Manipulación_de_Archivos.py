77fee1a243f54cf9ea1187b5510db4f33389bbfa
Maia Naidich <manaidich@gmail.com>
2021-04-20 15:49:53 -0300

    Fin Práctica Manipulación de Archivos

--- 4.4Práctica_Manipulación_de_Archivos.py
+++ 4.4Práctica_Manipulación_de_Archivos.py
@@ -111,3 +111,14 @@ for palabra in palabras:
 repeticiones_palabras
 
 #Ejercicio 10
+carpeta="F:\\Fundamentos_de_Informática\\Textos"
+texto_destino="F:\\Fundamentos_de_Informática\\Textos\\Texto_Destino"
+texto_a_combinar=""
+with open(carpeta,"r") as carpeta:
+    for texto in carpeta:
+        texto_leido=texto.read()
+        texto_a_combinar+=texto_leido
+        texto_a_combinar+="\n"
+    print(texto_a_combinar)
+with open(texto_destino,"w") as texto_destino:
+    texto_destino.write(texto_a_combinar)
